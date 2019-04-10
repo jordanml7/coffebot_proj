273ebdcba9412803c215d56a4a9805d0a7dd465b
Jordan Lueck <lueck.jordan@gmail.com>
2019-04-10 11:47:03 -0400

    created sound_test & fixed bugs

--- sound_test/src/sound_test.cpp
+++ sound_test/src/sound_test.cpp
@@ -18,8 +18,6 @@
 using namespace std;
 
 void sleepok(int, ros::NodeHandle &);
-void get_turtle_bot_loc(double home_location[3]);
-int move_turtle_bot (double, double, double);
 void sayPhrase(ros::Publisher, int, string, string);
 
 int main(int argc, char **argv)
@@ -32,19 +30,8 @@ int main(int argc, char **argv)
        
     //sleep for a bit to make sure the pub will work
     sleepok(2,n);
-    
-    // this will be reset based on starting location
-    double home_location[3] = {21.8,13.9,0.0};
-    
-    // coffee shop is currently right outside kitchenette
-    double coffee_shop[3] = {5.8,13.9,0.0};
   
     while (ros::ok()) {
-        
-        // RECORD STARTING LOCATION INTO HOME_LOCATION
-        get_turtle_bot_loc(home_location);
-        cout << "Starting at: " << home_location[0] << ", " << home_location[1];
-        // Maybe detect a person in a room and approach them, then record this location?
         
         sayPhrase(sound_pub,0,"","");
 	sleepok(2,n);
@@ -61,9 +48,6 @@ int main(int argc, char **argv)
         sayPhrase(sound_pub,2,name,coffee);
         sleepok(2,n);
         sleep(1);
-        move_turtle_bot(coffee_shop[0],coffee_shop[1],coffee_shop[2]);
-        sleepok(2,n);
-        sleep(1);
 
         sayPhrase(sound_pub,3,name,coffee);
         sleepok(2,n);
@@ -74,15 +58,11 @@ int main(int argc, char **argv)
         sayPhrase(sound_pub,4,name,coffee);
         sleepok(2,n);
         sleep(1);
-        move_turtle_bot(home_location[0],home_location[1],home_location[2]);
-        sleepok(2,n);
-        sleep(1);
         
         sayPhrase(sound_pub,5,name,coffee);
         sleepok(2,n);
         
         break;
-        // Update home_location to next person detected?
     }
     
     return 0;
@@ -93,58 +73,6 @@ void sleepok(int t, ros::NodeHandle &nh)
 {
     if (nh.ok())
         sleep(t);
-}
-
-void get_turtle_bot_loc(double home_location[3])
-{
-    tf::TransformListener listener;
-    
-    geometry_msgs::PoseStamped pBase, pMap;
-    pBase.header.frame_id = "base_link";
-    
-    pBase.pose.position.x = 0.0;
-    pBase.pose.position.y = 0.0;
-    pBase.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
-    
-    ros::Time current_transform = ros::Time::now();
-    listener.getLatestCommonTime(pBase.header.frame_id, "map", current_transform, NULL);
-    
-    pBase.header.stamp = current_transform;
-    listener.transformPose("map", pBase, pMap);
-    
-    home_location[0] = pMap[0];
-    home_location[1] = pMap[1];
-    home_location[2] = pMap[2];
-}
-
-int move_turtle_bot (double x, double y, double yaw)
-{
-
-    actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> ac("move_base",true);
-    ac.waitForServer();
-    move_base_msgs::MoveBaseGoal goal;
-    
-    cout << "Going to :" << x << ", " << y;
-
-    goal.target_pose.header.stamp = ros::Time::now();
-    goal.target_pose.header.frame_id = "/map";
-    
-    //set relative x, y, and angle
-    goal.target_pose.pose.position.x = x;
-    goal.target_pose.pose.position.y = y;
-    goal.target_pose.pose.position.z = 0.0;
-    goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(yaw);
-
-    //send the goal
-    ac.sendGoal(goal);
-    
-    //block until the action is completed
-    ac.waitForResult();
-    
-    //cout<<"\n \n waiting for " << sleep_time<<" seconds \n \n";
-    //sleep(sleep_time);
-  
-    return 0;
 }
 
 void sayPhrase(ros::Publisher sound_pub, int m, string name, string coffee)
