d53afce144919c8ffc63270bfc8e8f67d92bba07
Jordan Lueck <lueck.jordan@gmail.com>
2019-05-01 15:34:40 -0400

    more sensor tests

--- sensor_test/src/sensor_test.cpp
+++ sensor_test/src/sensor_test.cpp
@@ -9,21 +9,31 @@
 
 using namespace std;
 
+bool coffee;
+
 void sleepok(int, ros::NodeHandle &);
+void detect_coffee(const std_msgs::Bool& msg);
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "move_base_client");
     ros::NodeHandle n;
     
+    // subscriber for sensor msgs
+    ros::Subscriber coffee_detect = n.subscribe("/coffee_topic",1,detect_coffee);
+    //sleep for a bit to make sure the sub will work
+    sleepok(2,n);
 	
   
     while (ros::ok()) {
         
         ros::spinOnce();
-        
-        
-        break;
+        if (coffee)
+			cout << "Yep!" << endl;
+		else
+			cout << "Nope!" << endl;
+			
+		sleep(1);
     }
     
     return 0;
@@ -34,4 +44,9 @@ void sleepok(int t, ros::NodeHandle &nh)
 {
     if (nh.ok())
         sleep(t);
+}
+
+void detect_coffee(const std_msgs::Bool& msg)
+{
+	coffee = msg.data;
 }
